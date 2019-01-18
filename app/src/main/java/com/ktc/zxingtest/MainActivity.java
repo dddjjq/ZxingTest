f651ff07179ddb03f1ddb0a0ccc3233a17e8abe6
dddjjq <dingyunlong1996@hotmail.com>
2019-01-18 11:46:58 +0800

    基本扫描功能实现

--- app/src/main/java/com/ktc/zxingtest/MainActivity.java
+++ app/src/main/java/com/ktc/zxingtest/MainActivity.java
@@ -1,11 +1,12 @@
 package com.ktc.zxingtest;
 
 import android.Manifest;
-import android.content.ContentResolver;
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -14,13 +15,23 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.FormatException;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+import java.util.Hashtable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String[] PERMISSION_EXTERNAL_STORAGE = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE};
     private static final int REQUEST_EXTERNAL_STORAGE = 200;
+    private static final String INTENT_EXTRA_KEY_QR_SCAN = "scan";
+    private ProgressDialog mProgress;
+    private Bitmap scanBitmap;
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         readButton = findViewById(R.id.read);
+        message = findViewById(R.id.message);
     }
 
     private void addListener() {
@@ -58,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 100 && resultCode == RESULT_OK) {
-            Log.d("dingyl", "getRealPath : " + getRealPath(data.getData()));
+            handleAlbumPic(data);
         }
     }
 
@@ -82,8 +98,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void verifyStoragePermissions() {
-        Log.d("dingyl","verifyStoragePermissions : " +
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE));
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) { // 申请权限
             ActivityCompat.requestPermissions(MainActivity.this,
@@ -95,16 +109,74 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
+        switch (requestCode) {
             case REQUEST_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                if (grantResults.length>0&&grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    Log.d("dingyl","request 1");
-                }else {
-                    Log.d("dingyl","request 1");
-                    Toast.makeText(this,"授权失败",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
     }
+
+    private void handleAlbumPic(Intent data) { //获取选中图片的路径
+        final Uri uri = data.getData();
+        mProgress = new ProgressDialog(MainActivity.this);
+        mProgress.setMessage("正在扫描...");
+        mProgress.setCancelable(false);
+        mProgress.show();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Result result = scanningImage(uri);
+                mProgress.dismiss();
+                if (result != null) {
+                    /*Intent resultIntent = new Intent();
+                    Bundle bundle = new Bundle();
+                    bundle.putString(INTENT_EXTRA_KEY_QR_SCAN, result.getText());
+                    resultIntent.putExtras(bundle);
+                    MainActivity.this.setResult(RESULT_OK, resultIntent);
+                    finish();*/
+                    message.setText(result.getText());
+                    Log.d("dingyl","result : " + result.getText());
+                } else {
+                    Toast.makeText(MainActivity.this, "识别失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public Result scanningImage(Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+        String path = getRealPath(uri);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true; // 先获取原大小
+        scanBitmap = BitmapFactory.decodeFile(path, options);
+        options.inJustDecodeBounds = false;
+        int sampleSize = (int) (options.outHeight / (float) 200);
+        if (sampleSize <= 0) sampleSize = 1;
+        options.inSampleSize = sampleSize;
+        scanBitmap = BitmapFactory.decodeFile(path,options);
+        Log.d("dingyl","scanBitmap path : " + path);
+        Hashtable<DecodeHintType, String> hints = new Hashtable<>();
+        hints.put(DecodeHintType.CHARACTER_SET, "UTF8"); //设置二维码内容的编码
+        // scanBitmap = BitmapUtil.decodeUri(this, uri, 500, 500);
+        int data[] = new int[scanBitmap.getWidth() * scanBitmap.getHeight()];
+        scanBitmap.getPixels(data, 0, scanBitmap.getWidth(), 0, 0, scanBitmap.getWidth(),scanBitmap.getHeight());
+        RGBLuminanceSource source = new RGBLuminanceSource(scanBitmap.getWidth(), scanBitmap.getHeight(), data);
+        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+        try {
+            return reader.decode(bitmap1, hints);
+        } catch (NotFoundException | ChecksumException | FormatException e) {
+            Log.d("dingyl", "exception : " + e.getCause());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
